767de6a35b64b458513e0bfe690483f33f8f1fb4
Topher DeRosia <topher@mediaforge.pro>
2026-08-16 13:56:58 -0700

    Add Michelle Frechette as a contributor

--- wp-photo-directory-importer.php
+++ wp-photo-directory-importer.php
@@ -6,7 +6,7 @@
  * Version:           1.3.3
  * Requires at least: 5.8
  * Requires PHP:      7.4
- * Author:            ekamran, veeeharris, mattgaldino, telizarose, topher1kenobe, gusteci
+ * Author:            ekamran, veeeharris, mattgaldino, telizarose, topher1kenobe, gusteci, michelleames
  * Author URI:        https://github.com/your-username
  * License:           GPL v2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
